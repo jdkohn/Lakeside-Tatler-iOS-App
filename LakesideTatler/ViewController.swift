d06510887190262fd0ac97f9ec02d25151ce7968
jdkohn <jdkohn18@gmail.com>
2016-03-07 10:49:19 -0800

    Initial Commit

---
+++ LakesideTatler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LakesideTatler
+//
+//  Created by Jacob Kohn on 3/7/16.
+//  Copyright © 2016 Jacob Kohn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
