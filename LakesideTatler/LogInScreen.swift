39c8d44442923abbad2148a0c846be9bfc9a2512
jdkohn <jdkohn18@gmail.com>
2016-03-16 09:25:14 -0700

    WPXMLRPC Pod Installed

--- LakesideTatler/LogInScreen.swift
+++ LakesideTatler/LogInScreen.swift
@@ -29,14 +29,19 @@ class LogInViewController: UIViewController {
     
     func logIn(sender: UIButton) {
         
+        //get the contents of the text fields
         let username = usernameField.text!
         let password = passwordField.text!
         
         var responseString = "" as! NSString
         
+        /*
+        * Make a POST request to the login page
+        * posts username and password entered and submits the form
+        */
         let request = NSMutableURLRequest(URL: NSURL(string: "http://tatler.lakesideschool.org/wp-login.php")!)
         request.HTTPMethod = "POST"
-        let postString = "log=" + username + "&pwd=" + password + "&wp-submit=Log%20In&redirect_to=http://tatler.lakesideschool.org/"
+        let postString = "log=" + username + "&pwd=" + password + "&wp-submit=Log%20In"
         request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
         let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
             guard error == nil && data != nil else {            // check for fundamental networking error
@@ -52,21 +57,33 @@ class LogInViewController: UIViewController {
             responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
             //print("responseString = \(responseString)")
             
+            //dispatch_async function runs after the request is made
+            
             dispatch_async(dispatch_get_main_queue()) {
+                
+                //checks to see if the page post-login contains a 'login error'
                 if((responseString).rangeOfString("<div id=\"login_error\">").location == NSNotFound) {
-                    print("Log In Success!")
+                    
+                    let alert = UIAlertController(title: "Success!", message: "You have logged in to The Tatler", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: .Default, handler: { (action) -> Void in
+                        
+                    }))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    
                     self.usernameField.text! = ""
                     self.passwordField.text! = ""
                 } else {
-                    print("Log In Fail")
-                    self.usernameField.text! = ""
-                    self.passwordField.text! = ""
+                    let alert = UIAlertController(title: "Awww :(", message: "Incorrect Username/Password", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: .Default, handler: { (action) -> Void in
+                
+                    }))
+                    self.presentViewController(alert, animated: true, completion: nil)
+//                    self.usernameField.text! = ""
+//                    self.passwordField.text! = ""
                 }
             }
             
         }
         task.resume()
-        
-        
     }
 }
---
+++ Pods/Target Support Files/wpxmlrpc/wpxmlrpc-umbrella.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+#import "WPXMLRPC.h"
+#import "WPXMLRPCEncoder.h"
+#import "WPXMLRPCDecoder.h"
+
+FOUNDATION_EXPORT double wpxmlrpcVersionNumber;
+FOUNDATION_EXPORT const unsigned char wpxmlrpcVersionString[];
+
